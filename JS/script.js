ed749fd39dbce6445258c5c0fcdd15298cb1e5fe
Évelim Dornelles <dorrrnelles@gmail.com>
2024-09-23 08:43:29 -0300

    versão final

--- JS/script.js
+++ JS/script.js
@@ -2,4 +2,10 @@ const doutora = document.getElementById('doutora')
 
 doutora.addEventListener('mouseover', function () {
     doutora.src = "IMG/foto2.png"
-})
+
+
+});
+
+doutora.addEventListener('mouseout', function () {
+    doutora.src = "IMG/foto1 (1).png"
+});
